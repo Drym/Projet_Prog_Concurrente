5ccaefe93d2178b02d624eaf3f4f6abc4e735520
Chloé GUGLIELMI <guglielmi.chloe@etu.unice.fr>
2016-02-02 11:44:00 +0100

    entrer params

--- Main.c
+++ Main.c
@@ -2,4 +2,23 @@
 // Created by lucas on 02/02/16.
 //
 
+#include <stdio.h>
 #include "Main.h"
+
+
+int main(){
+	demander_param();
+	return 0;
+}
+
+int demander_param(){
+	int c;
+    
+    printf("Veuillez taper un entier : ");
+    c = getc(stdin);
+
+    printf("Vous avez tape : ");
+    putc(c, stdout);
+
+    return 0;
+}
--- Main.h
+++ Main.h
@@ -5,4 +5,6 @@
 #ifndef PROJET_PROG_CONCURRENTE_MAIN_H
 #define PROJET_PROG_CONCURRENTE_MAIN_H
 
+int demander_param();
+
 #endif //PROJET_PROG_CONCURRENTE_MAIN_H
